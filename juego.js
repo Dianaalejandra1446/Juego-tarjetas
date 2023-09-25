c6d12deb1553627260abd8b522a7926f50278613
Dianaalejandra1446 <dianaleja04@hotmail.com>
2023-09-25 11:43:15 -0500

    Juego avanze

---
+++ juego.js
@@ -0,0 +1,36 @@
+var num_aletorio = Math.random(1,11);
+var num_aletorio2 = Math.random(1,11);
+
+if (num_aletorio + num_aletorio2 == 2 ){
+
+}
+else if (num_aletorio + num_aletorio2 == 3) {
+
+}
+else if (num_aletorio + num_aletorio2 == 5) {
+
+}
+else if (num_aletorio + num_aletorio2 == 8) {
+
+}
+else if (num_aletorio + num_aletorio2 == 13) {
+
+}
+else if (num_aletorio + num_aletorio2 == 21) {
+
+} else {
+    alert("No sacaste carta :( ")
+}
+
+
+/* let carta1 = 1;
+let carta2 = 2;
+let carta3 = 3;
+let carta4 = 4;
+let carta5 = 5;
+let carta6 = 6;
+let carta7 = 7;
+let carta8 = 8;
+let carta9 = 9;
+let carta10 = 10;
+let carta11 = 11; */
